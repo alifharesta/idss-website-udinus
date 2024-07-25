95d1df53507b577d58b89ff16ed0c5bae72a9dcd
alipo <111202113768@mhs.dinus.ac.id>
2024-07-25 09:47:19 +0700

    progress

--- src/components/LandingPage/HeroSection.jsx
+++ src/components/LandingPage/HeroSection.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 // import bghero from '../../assets/bghero.svg'
 export default function HeroSection() {
   return (
-    <section className='container mx-auto xl:px-52'>
-      <div className='grid grid-cols-2 md:grid-cols-2 md:mt-10 items-center text-left'>
-        
+    <section className="container mx-auto xl:px-48 absolute top-1/3">
+      <div className="container grid grid-cols-1 md:grid-cols-2 mx-auto items-center">
+        <div className="order-2 md:order-1">
+          <h1 className="text-4xl font-bold mb-4 text-white px-10 top-0">
+            IDSS<br></br> (Intelligent Distributed Surveillance and Security)
+          </h1>
+        </div>
       </div>
     </section>
-  )
+  );
 }
